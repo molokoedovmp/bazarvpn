a18100a6277e195cbccb1a074069da390ee62876
Mikhail Molokoedov <molokoedovmp@gmail.com>
2026-01-01 14:56:51 +0300

    Fix payment result build error by removing revalidate exports from client page

--- landing/app/payment/result/page.tsx
+++ landing/app/payment/result/page.tsx
@@ -1,10 +1,4 @@
 "use client";
-"use client";
-
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-export const fetchCache = "force-no-store";
-
 import Link from "next/link";
 import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
